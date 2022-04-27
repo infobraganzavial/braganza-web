397d2cedf2ed9fcb1e960595d740f7e472663b95
Jose Felipe Gonzalez <josefelipe25abril@gmail.com>
2022-04-27 00:45:56 -0300

    Agregar mejoras en OurProjects, Contact Us y AboutUs

--- src/components/aboutUs/AboutUs.jsx
+++ src/components/aboutUs/AboutUs.jsx
@@ -7,7 +7,7 @@ const AboutUsContainer = styled.section`
     position: relative;
     width: 100%;
     height: 100%;
-    min-height: 100vh;
+    min-height: calc(100vh - 100px);
     background-position: center;
     background-repeat: no-repeat;
     background-size: cover;
--- src/components/ourProjects/OutProjects.jsx
+++ src/components/ourProjects/OutProjects.jsx
@@ -13,7 +13,7 @@ const HexagonWrapper = styled.article`
     position: relative;
     width: 100%;
     height: 100%;
-    min-height: 100vh;
+    min-height: calc(100vh - 100px);
     background-position: center;
     background-repeat: no-repeat;
     background-size: cover;
