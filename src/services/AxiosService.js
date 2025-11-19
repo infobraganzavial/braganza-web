09a46ac46e7021c959a108824e000267bf9b1fe6
Jabc <develsoft.jabc@gmail.com>
2025-11-19 16:39:51 -0400

    fix(apiURL): change api url

--- src/services/AxiosService.js
+++ src/services/AxiosService.js
@@ -1,36 +1,36 @@
-import axios from 'axios';
+import axios from "axios";
 
-const BASE_URL = process.env.URL_API || 'https://braganza-api-env-production.herokuapp.com/api/v1/';
+const BASE_URL = process.env.URL_API || "https://braganza-api.onrender.com/";
 
-console.log(BASE_URL, "BASE_URL")
+console.log(BASE_URL, "BASE_URL");
 
 const api = axios.create({
-    baseURL: BASE_URL,
+  baseURL: BASE_URL + "api/v1/",
 });
 
 api.interceptors.request.use(
-    (req) => {
-        // TODO: Agregar lista de request con headers y tokens.
-        // Do something before request is sent
-        return req;
-    },
-    function (error) {
-        // Do something with request error
-        return Promise.reject(error);
-    }
+  (req) => {
+    // TODO: Agregar lista de request con headers y tokens.
+    // Do something before request is sent
+    return req;
+  },
+  function (error) {
+    // Do something with request error
+    return Promise.reject(error);
+  }
 );
 
 api.interceptors.response.use(
-    (response) => {
-        // Any status code that lie within the range of 2xx cause this function to trigger
-        // Do something with response data
-        return response;
-    },
-    function (error) {
-        // Any status codes that falls outside the range of 2xx cause this function to trigger
-        // Do something with response error
-        return Promise.reject(error);
-    }
+  (response) => {
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    // Do something with response data
+    return response;
+  },
+  function (error) {
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
+    return Promise.reject(error);
+  }
 );
 
 const { get, post, put } = api;
