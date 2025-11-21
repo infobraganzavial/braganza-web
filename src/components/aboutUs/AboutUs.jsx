fca73c7cce299d6cf1572d811ccfe30e64a05db3
Jabc <develsoft.jabc@gmail.com>
2025-11-21 15:01:23 -0400

    fix(newImgs): add new images urls

--- src/components/aboutUs/AboutUs.jsx
+++ src/components/aboutUs/AboutUs.jsx
@@ -1,102 +1,129 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 // Theming
-import colors from '../../theming/colors';
+import colors from "../../theming/colors";
 
 const Wrapper = styled.div`
-    position: relative;
-`
+  position: relative;
+`;
 
 const IdDiv = styled.div`
-    position: absolute;
-    width: 100%;
-    height: calc(100vh - 102px);
-`
+  position: absolute;
+  width: 100%;
+  height: calc(100vh - 102px);
+`;
 
 const AboutUsContainer = styled.section`
-    position: relative;
-    width: 100%;
-    height: 100%;
-    min-height: calc(100vh - 100px);
-    background-position: center;
-    background-repeat: no-repeat;
-    background-size: cover;
-    background-attachment: fixed;
-    display: flex;
+  position: relative;
+  width: 100%;
+  height: 100%;
+  min-height: calc(100vh - 100px);
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-attachment: fixed;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+  padding: 5rem;
+
+  @media (max-width: 1200px) {
     flex-direction: column;
     justify-content: center;
-    padding: 5rem;
-`
+  }
+`;
+
+const ImageISO = styled.div`
+  flex: 1;
+  width: 100%;
+  height: 800px;
+  min-height: 600px;
+
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: contain;
+`;
 
 const Service = styled.article`
-    display: flex;
-    position: relative;
-    width: 40%;
-    min-width: 340px;
-    height: auto;
-    min-height: max-content;
-    max-height: max-content;
-    margin: 1rem 0;
-    flex-direction: column;
-    background-color: #fff;
+  display: flex;
+  position: relative;
+  width: 40%;
+  min-width: 340px;
+  height: auto;
+  min-height: max-content;
+  max-height: max-content;
+  margin: 1rem 0;
+  flex-direction: column;
+  background-color: #fff;
 
-    & section:first-of-type h2 {
-        font-size: 2rem;
-    }
-`
+  & section:first-of-type h2 {
+    font-size: 2rem;
+  }
+`;
 
 const Info = styled.section`
-    display: flex;
-    flex-direction: column;
-    justify-content: space-around;
-    color: ${colors.darkColor};
-    width: 100%;
-    padding: 1rem;
-    
-    & h2, p {
-        font-size: 1rem;
-        text-transform: uppercase;
-        text-align: center;
-        white-space: pre-line;
-    }
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  color: ${colors.darkColor};
+  width: 100%;
+  padding: 1rem;
 
-    & h2 {
-        font-size: 1.5rem;
-        color: ${colors.primaryColor};
-    }
-`
+  & h2,
+  p {
+    font-size: 1rem;
+    text-transform: uppercase;
+    text-align: center;
+    white-space: pre-line;
+  }
 
-const AboutUs = ({aboutUs}) => {
+  & h2 {
+    font-size: 1.5rem;
+    color: ${colors.primaryColor};
+  }
+`;
 
-    const buildServices = () =>{
-        return (
-            <Service className='aboutus__service'>
-                {
-                    aboutUs.content.map( s => {
-                        return (
-                            <Info className='aboutus__info'
-                                  key={s.id} 
-                                  length={aboutUs.content.length}>
-                                <h2>{s.title}</h2>
-                                <p>{s.description}</p>
-                            </Info>
-                        )
-                    })
-                }
-            </Service>
-        )
-    }
-
+const AboutUs = ({ aboutUs }) => {
+  const buildServices = () => {
     return (
-        <Wrapper>
-            <IdDiv id='sobre-nosotros'></IdDiv>
-            <AboutUsContainer
-                                className='about-us__containner'
-                                style={{ backgroundImage: `linear-gradient(to bottom, rgba(245, 246, 252, 0.3), rgba(0, 0, 0, 0.1)), url(${aboutUs.imageBg.url})`}}>
-                { buildServices() }
-            </AboutUsContainer>
-        </Wrapper>
-        )
-}
+      <Service className="aboutus__service">
+        {aboutUs.content.map((s) => {
+          return (
+            <Info
+              className="aboutus__info"
+              key={s.id}
+              length={aboutUs.content.length}
+            >
+              <h2>{s.title}</h2>
+              <p>{s.description}</p>
+            </Info>
+          );
+        })}
+      </Service>
+    );
+  };
+
+  return (
+    <Wrapper>
+      <IdDiv id="sobre-nosotros"></IdDiv>
+      <AboutUsContainer
+        className="about-us__containner"
+        style={{
+          backgroundImage: `linear-gradient(to bottom, rgba(245, 246, 252, 0.3), rgba(0, 0, 0, 0.1)), url(${aboutUs.imageBg.url})`,
+        }}
+      >
+        {buildServices()}
+
+        <ImageISO
+          style={{
+            backgroundImage:
+              "url(https://res.cloudinary.com/dc2jukw2z/image/upload/v1763750820/iso_zspavg.webp)",
+          }}
+        />
+      </AboutUsContainer>
+    </Wrapper>
+  );
+};
 
 export default AboutUs;
--- src/components/imagesHoverEffect/ImagesHoverEffect.jsx
+++ src/components/imagesHoverEffect/ImagesHoverEffect.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import { useState } from 'react';
+import React from "react";
+import { useState } from "react";
 
-import styled, { css, keyframes } from 'styled-components';
+import styled, { css, keyframes } from "styled-components";
 
 const fadeInLeft = keyframes`
     from {
@@ -10,139 +10,144 @@ const fadeInLeft = keyframes`
     to {
         opacity: 1;
     }
-`
+`;
 
 const Toggle = styled.div`
-    position: absolute;
-    justify-content: center;
-    align-items: center;
-    border-radius: 50%;
-    width: 45px;
-    height: 45px;
-    z-index: 10;
-    right: 10px;
-    top: 10px;
-    background-color: #fff;
-    display: none;
-    ${({isActive}) => isActive && css`
-        display: flex;
+  position: absolute;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50%;
+  width: 45px;
+  height: 45px;
+  z-index: 10;
+  right: 10px;
+  top: 10px;
+  background-color: #fff;
+  display: none;
+  ${({ isActive }) =>
+    isActive &&
+    css`
+      display: flex;
     `}
-    animation: ${fadeInLeft} .5s;
-`
+  animation: ${fadeInLeft} .5s;
+`;
 
 const DivClip = styled.div`
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    transition: .5s;
-    filter: grayscale(100%);
-`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  transition: 0.5s;
+  filter: grayscale(100%);
+`;
 
 const Wrapper = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: start;
+  min-height: calc(100vh - 95px);
+  overflow: hidden;
+  /* background: ${({ theme }) => theme.primaryColor}; */
+
+  & .services__title {
+    padding: 1rem;
+    color: ${({ theme }) => theme.textDark};
+    width: 40%;
+    background: rgba(255, 255, 255, 0.8);
+  }
+
+  & .section__container {
     position: relative;
-    display: flex;
-    justify-content: center;
-    align-items: start;
-    min-height: calc(100vh - 95px);
-    overflow: hidden;
-    /* background: ${({theme}) => theme.primaryColor}; */
+    width: 100%;
+    height: 100%;
+    min-height: inherit;
+    /* background: ${({ theme }) => theme.primaryColor}; */
+  }
 
-    & .services__title {
-        padding: 1rem;
-        color: ${({theme}) => theme.textDark};
-        width: 40%;
-        background: rgba(255,255,255,.8);
+  & .section__container:hover .clip {
+    clip-path: polygon(100% 0, 100% 0, 100% 100%, 100% 100%);
+  }
+
+  & .section__container .clip {
+    &:hover {
+      clip-path: polygon(0 0, 100% 0, 100% 100%, 0 100%);
+      filter: grayscale(0);
     }
 
-    & .section__container  {
-        position: relative;
-        width: 100%;
-        height: 100%;
-        min-height: inherit;
-        /* background: ${({theme}) => theme.primaryColor}; */
+    &:hover .content {
+      bottom: 0;
+      opacity: 1;
     }
+  }
 
-    & .section__container:hover .clip {
-        clip-path: polygon(100% 0, 100% 0, 100% 100%, 100% 100%);
-    }
+  & .clip .content {
+    position: absolute;
+    bottom: -100%;
+    left: 0;
+    width: 70%;
+    padding: 20px;
+    background: #fff;
+    opacity: 0;
+    transition: 0.5s;
+  }
 
-    & .section__container .clip {
-        &:hover {
-            clip-path: polygon(0 0, 100% 0, 100% 100%, 0 100%);
-            filter: grayscale(0);
-        }
+  & .clip.clip1 {
+    background-size: cover;
+    background-position: center;
+    clip-path: polygon(0 0, 55% 0, 20% 100%, 0% 100%);
+  }
 
-        &:hover .content{
-            bottom: 0;
-            opacity: 1;
-        }
-    }
+  & .clip.clip2 {
+    background-size: cover;
+    background-position: center;
+    clip-path: polygon(55% 0, 100% 0, 45% 100%, 20% 100%);
+  }
 
-    & .clip .content {
-            position: absolute;
-            bottom: -100%;
-            left: 0;
-            width: 70%;
-            padding: 20px;
-            background: #fff;
-            opacity: 0;
-            transition: .5s;
-    }
+  & .clip.clip3 {
+    background-size: cover;
+    background-position: center;
+    clip-path: polygon(100% 0, 100% 0, 100% 100%, 45% 100%);
+  }
+`;
 
-    & .clip.clip1 {
-        background-size: cover;
-        background-position: center;
-        clip-path: polygon(0 0, 55% 0, 20% 100%, 0% 100%);
-    }
+const ImagesHoverEffect = ({ sections }) => {
+  const [active, setActive] = useState(false);
 
-    & .clip.clip2 {
-        background-size: cover;
-        background-position: center;
-        clip-path: polygon(55% 0, 100% 0, 45% 100%, 20% 100%);
-    }
-
-    & .clip.clip3 {
-        background-size: cover;
-        background-position: center;
-        clip-path: polygon(100% 0, 100% 0, 100% 100%, 45% 100%);
-    }
-`
-
-const ImagesHoverEffect = ({sections}) => {
-
-    const [active, setActive] = useState(false);
-
-    const buildSection = () => {
-            return sections.map( (c, index) => {
-            return <DivClip key={c.id} 
-                        className={`clip clip${index + 1}`} 
-                        style={{ backgroundImage: `url(${c.principalImage.url})` }}
-                        onTouchStart={ (e) => setActive(true) }
-                        >
-                    <h2 className='services__title'>Nuestros Servicios</h2>
-                    <div className='content'>
-                        <h2>{c.title}</h2>
-                        <p>{c.description}</p>
-                    </div>
-                </DivClip>
-            })
-                    
-        }
-        return (
-            <Wrapper id='nuestros-servicios'>
-            <Toggle
-                onClick={() => setActive(false)}
-                isActive={active}
-            >
-                <i className="fa-solid fa-xmark"></i>
-            </Toggle>
-            <div className='section__container'>
-                {buildSection()}
-            </div>
-        </Wrapper>
-    )
-}
+  const buildSection = () => {
+    return sections.map((c, index) => {
+      return (
+        <DivClip
+          key={c.id}
+          className={`clip clip${index + 1}`}
+          style={{ backgroundImage: `url(${c.principalImage.url})` }}
+          onTouchStart={(e) => setActive(true)}
+        >
+          <h2 className="services__title">Nuestros Servicios</h2>
+          <div className="content">
+            <h2>{c.title}</h2>
+            <p>{c.description}</p>
+          </div>
+        </DivClip>
+      );
+    });
+  };
+  return (
+    <Wrapper id="nuestros-servicios">
+      <Toggle onClick={() => setActive(false)} isActive={active}>
+        <i className="fa-solid fa-xmark"></i>
+      </Toggle>
+      <div
+        className="section__container"
+        style={{
+          backgroundImage: `url(https://res.cloudinary.com/dc2jukw2z/image/upload/v1763750986/firm-handshake_rzw1ya.webp)`,
+        }}
+      >
+        {buildSection()}
+      </div>
+    </Wrapper>
+  );
+};
 
 export default ImagesHoverEffect;
